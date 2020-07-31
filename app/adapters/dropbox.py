2773f744e634d8c72c2dc2fe0138f880a229fed7
Maurizio Branca <m.branca@arduino.cc>
2020-07-31 07:33:41 +0200

    Dropbox: upgrade to file search v2

--- app/adapters/dropbox.py
+++ app/adapters/dropbox.py
@@ -11,10 +11,16 @@ class DropboxAdapter:
         self.root_folder = root_folder
         self.client = dropbox.Dropbox(access_token)
 
-    def exists(self, key: str):
-        # print('checking for {folder}/{key}'.format(key=key, folder=self.root_folder))
-        return self.client.files_search(self.root_folder, key).matches
+    def exists(self, query: str) -> bool:
+        options = dropbox.files.SearchOptions(
+            path=self.root_folder
+        )
+        return len(self.client.files_search_v2(query, options).matches) > 0
 
     def put_file(self, file_path: str, file_name: str):
         with open(file_path, "rb") as f:
-            self.client.files_upload(f.read(), '%s/%s' % (self.root_folder, file_name), mute = True)
+            self.client.files_upload(
+                f.read(),
+                f'{self.root_folder}/{file_name}',
+                mute=True
+            )
